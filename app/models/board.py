6f43bbfe02dc94fb6ee2ed20c3e6bfe050a17033
Bogyeong Kim <bogyeongkim614@gmail.com>
2024-05-26 16:57:36 +0900

    Revert "fix: board 테이블 구조 수정"
    This reverts commit 8a204ba8afbaf710e612837c2070bc7d7fe4c5ba.

--- app/models/board.py
+++ app/models/board.py
@@ -1,13 +1,12 @@
-from sqlalchemy import Table, Column, BigInteger, String, TIMESTAMP, MetaData, text
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
 metadata = MetaData()
 
 board = Table(
     "board",
     metadata,
-    Column("id", BigInteger, primary_key=True, autoincrement=True),
-    Column("title", String(255), nullable=False),
-    Column("contents", String(255), nullable=False),
-    Column("created_by", String(255), nullable=False),
-    Column("created_at", TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'), nullable=True),
+    Column("id", Integer, primary_key=True),
+    Column("title", String(255)),
+    Column("contents", String(255)),
+    Column("created_by", String(50)),
 )
--- app/schemas/board.py
+++ app/schemas/board.py
@@ -1,9 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
 
 class Board(BaseModel):
     title: str
     contents: str
     created_by: str
-    created_at: Optional[datetime] = None
