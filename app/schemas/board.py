a885a4ce424cb265b15d075e31ef3c0d243d7f3f
Bogyeong Kim <bogyeongkim614@gmail.com>
2024-05-26 17:00:36 +0900

    fix: board 테이블 구조 변경

--- app/schemas/board.py
+++ app/schemas/board.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
 
 class Board(BaseModel):
     title: str
     contents: str
     created_by: str
+    created_at: Optional[datetime] = None
